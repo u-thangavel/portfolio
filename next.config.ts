fe467362597f3eab20cdc89c3a32f1fed6316c9b
u-thangavel <u.thangavel1990@gmail.com>
2026-08-01 14:45:24 +0530

    Added GitHub Actions workflow for static deploymen t

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/thangavel.github.io',
+  assetPrefix: '/thangavel.github.io/',
   images: {
     unoptimized: true,
   },
